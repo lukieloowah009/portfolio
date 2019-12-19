dccfec9421ec096400720ef088264a68814cfdcd
Saw Luke Loo Wah <>
2019-12-19 16:39:06 -0500

    Updates

---
+++ precache-manifest.a2edb168a844c43fc4a19c4486be5af9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06429ea5ee9f3164a272efb252a73285",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b958d7b1ebd2c577e6f6",
+    "url": "/portfolio/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "fd4e0e56d59abc1c1e3b",
+    "url": "/portfolio/static/css/main.a065eb7c.chunk.css"
+  },
+  {
+    "revision": "b958d7b1ebd2c577e6f6",
+    "url": "/portfolio/static/js/2.ab01d862.chunk.js"
+  },
+  {
+    "revision": "fd4e0e56d59abc1c1e3b",
+    "url": "/portfolio/static/js/main.76a5340b.chunk.js"
+  },
+  {
+    "revision": "77f1202a340ffca9f6aa",
+    "url": "/portfolio/static/js/runtime-main.75e3564e.js"
+  },
+  {
+    "revision": "8505753fe32df8aff30219c8fb42d2ec",
+    "url": "/portfolio/static/media/HEADSHOT.8505753f.jpeg"
+  }
+]);
